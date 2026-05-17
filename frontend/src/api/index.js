c6ebf595c19680daf71823d1686b482eaf785a87
Yasir Sultan <sultanyasir990@gmail.com>
2026-05-17 21:46:48 +0500

    Updated backend url

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://mus-welfare.vercel.app/api'
 });
 
 // Attach JWT + smart Content-Type (don't override for FormData)
